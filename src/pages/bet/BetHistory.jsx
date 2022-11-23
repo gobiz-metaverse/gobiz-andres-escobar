6c7781bf30c5dbb2e51a5d2d12dac55b4b1a54be
kiennx <nguyenxuankien@gobiz.vn>
2022-11-23 18:17:33 +0700

    fix report height

--- src/pages/bet/BetHistory.jsx
+++ src/pages/bet/BetHistory.jsx
@@ -4,7 +4,7 @@ import StandardLayout from "../../layouts/StandardLayout";
 export default class BetHistory extends React.Component {
     render() {
         return <StandardLayout><iframe width="100%"
-                       height={'500px'}
+                       height={'800px'}
                 src="https://datastudio.google.com/embed/reporting/af3206e5-17b8-414b-a4dd-63be89c202d2/page/p_9jeuyals0c"
                 frameBorder="0" allowFullScreen></iframe>
         </StandardLayout>
--- src/pages/bet/TopPlaying.jsx
+++ src/pages/bet/TopPlaying.jsx
@@ -5,7 +5,7 @@ export class TopPlaying extends React.Component {
     render() {
         return <StandardLayout>
             <iframe width="100%"
-                    height={'500px'}
+                    height={'800px'}
                        src="https://datastudio.google.com/embed/reporting/af3206e5-17b8-414b-a4dd-63be89c202d2/page/bnV8C"
                        frameBorder="0" allowFullScreen></iframe>
         </StandardLayout>
